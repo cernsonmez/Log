a27f6754c72edc9c07a1fcda99fa60a849a4e05b
Ceren Sönmez <cerensonmez95@gmail.com>
2016-09-01 02:55:26 +0300

    Add files via upload

---
+++ Log.DataTypes/LogInfo.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Log.DataTypes
+{
+    public enum LogType
+    {
+        Debug,
+        Info,
+        Fatal,
+        Error,
+        Trace,
+        Warn
+    };
+
+    [Serializable]
+    public class LogInfo
+    {
+        public LogInfo(LogType type, int sequenceid, string appname, string message)
+        {
+            this.LogId = Guid.NewGuid().ToString();
+            this.TimeStamp = DateTime.Now;
+            this.Type = type;
+            this.AppName = appname;
+            this.Message = message;
+            this.SequenceID = sequenceid;
+
+        }
+
+        [BsonId]
+        private string LogId { get; set; }
+
+        [BsonElement("sequence")]
+        public int SequenceID { get; set; }
+
+        [BsonElement("appname")]
+        public string AppName { get; set; }
+
+        [BsonElement("timestamp")]
+        public DateTime TimeStamp { get; set; }
+
+        [BsonElement("type")]
+        public LogType Type { get; set; }
+
+        [BsonElement("message")]
+        public string Message { get; set; }
+
+    }
+}
