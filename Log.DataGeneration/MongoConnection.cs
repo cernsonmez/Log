6408236267a0dca6af8aae52f178fcb11eb83880
Ceren Sönmez <cerensonmez95@gmail.com>
2016-09-01 02:55:57 +0300

    Add files via upload

---
+++ Log.DataGeneration/MongoConnection.cs
@@ -0,0 +1,53 @@
+﻿using Log.DataTypes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+
+namespace Log.DataGeneration
+{
+    public interface IDatabaseConnection
+    {
+        void SaveDocument(LogInfo logMessage);
+    }
+
+    public class MongoConnection : IDatabaseConnection
+    {
+        private MongoClient _client;
+        private IMongoCollection<LogInfo> _collection;
+        public MongoConnection(string connector, string databaseName, string colName)
+        {
+            _client = new MongoClient(@connector);
+            var database = _client.GetDatabase(databaseName);
+            _collection = database.GetCollection<LogInfo>(colName);
+        }
+        public void SaveDocument(LogInfo logMessage)
+        {
+            _collection.InsertOne(logMessage);
+
+        }
+        public void Querys()
+        {
+            /////////////////////////////////////////////////////sorgu işlemleri
+            var retrievedDocument = _collection.Find(new BsonDocument()).FirstOrDefault();
+
+            //Veritabani kayıt sayısı
+            var count = _collection.Count(new BsonDocument());
+            Console.WriteLine(count);
+
+            //Filtreleme yöneticisi oluşturma:
+            var filterBuilder = Builders<LogInfo>.Filter;
+
+            //Sorgulama:
+            var filter = filterBuilder.Eq("appname", "SFS") & filterBuilder.Eq("sequence", 12);
+            var retrievedDoc = _collection.Find(filter).ToList();
+
+            //tarih sorgulama:
+            var dateFilter = filterBuilder.Gte("timestamp", new DateTime(2016, 8, 1));
+
+            //filtre uygulama:
+            //var retrievedDoc = collection.Find(dateFilter).ToList();
+        }
+
+    }
+}
---
+++ Log.DataGeneration/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ninject;
+using System;
+using System.Reflection;
+
+namespace Log.DataGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            var logic = new ServiceLogic(kernel.Get<IConsumerConnection<LogInfo>>(), kernel.Get<IServiceConnection<LogInfo>>(), kernel.Get<IDatabaseConnection>());
+            logic.InitiateService();
+
+            Console.ReadKey();
+        }
+    }
+}
